c99b1b1b39154ed24c908dd2c3abf19baffec9eb
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-05-14 11:26:19 +0300

    feat: add a page theme change function

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,16 @@ function getFormattedDate(date) {
 }
 
 getFormattedDate(Date.now());
+
+const themeButton = document.querySelector(".checkbox__real");
+
+const changeColorScheme = () => {
+  if (colorScheme.getAttribute("href") == "./color-scheme/light.css") {
+    colorScheme.href = "./color-scheme/dark.css";
+  } else {
+    colorScheme.href = "./color-scheme/light.css";
+  }
+};
+
+const colorScheme = document.querySelector("#color-scheme");
+themeButton.addEventListener("click", changeColorScheme);
